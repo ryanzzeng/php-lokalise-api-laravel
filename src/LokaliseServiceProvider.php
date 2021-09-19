1e52619fa4db1c1c4ac7b1a6104bd7728bc983fd
Ryan Zeng <zengruijiang@gmail.com>
2021-09-19 14:22:25 +1000

    add all

---
+++ src/LokaliseServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Ryanzeng\Lokalise;
+
+use Illuminate\Support\ServiceProvider;
+
+class LokaliseServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->app->bind(LokaliseApiClient::class, function ($app) {
+            return new LokaliseApiClient($app['config']['services.lokalise.key']);
+        });
+    }
+}
